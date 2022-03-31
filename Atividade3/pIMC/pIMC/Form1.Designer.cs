e0e75881740fe6fa0193337c188489315b6314c5
Matheus Tadao Momiy <matheus.tadao@gmail.com>
2022-03-31 15:29:43 -0300

    Add files via upload

---
+++ Atividade3/pIMC/pIMC/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace pIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.mskbxAltura = new System.Windows.Forms.MaskedTextBox();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.mskbxPeso = new System.Windows.Forms.MaskedTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(26, 25);
+            this.lblAltura.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(68, 25);
+            this.lblAltura.TabIndex = 0;
+            this.lblAltura.Text = "Altura";
+            // 
+            // mskbxAltura
+            // 
+            this.mskbxAltura.Location = new System.Drawing.Point(120, 19);
+            this.mskbxAltura.Name = "mskbxAltura";
+            this.mskbxAltura.Size = new System.Drawing.Size(100, 31);
+            this.mskbxAltura.TabIndex = 1;
+            this.mskbxAltura.Validated += new System.EventHandler(this.mskbxAltura_Validated);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Location = new System.Drawing.Point(26, 82);
+            this.lblPeso.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(61, 25);
+            this.lblPeso.TabIndex = 0;
+            this.lblPeso.Text = "Peso";
+            // 
+            // mskbxPeso
+            // 
+            this.mskbxPeso.Location = new System.Drawing.Point(120, 76);
+            this.mskbxPeso.Name = "mskbxPeso";
+            this.mskbxPeso.Size = new System.Drawing.Size(100, 31);
+            this.mskbxPeso.TabIndex = 2;
+            this.mskbxPeso.Validated += new System.EventHandler(this.mskbxPeso_Validated);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkGray;
+            this.btnCalcular.Location = new System.Drawing.Point(32, 154);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(116, 38);
+            this.btnCalcular.TabIndex = 3;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.BackColor = System.Drawing.Color.DarkGray;
+            this.btnFechar.Location = new System.Drawing.Point(180, 154);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(116, 38);
+            this.btnFechar.TabIndex = 4;
+            this.btnFechar.Text = "Fechar";
+            this.btnFechar.UseVisualStyleBackColor = false;
+            this.btnFechar.Click += new System.EventHandler(this.btnFechar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(332, 233);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.mskbxPeso);
+            this.Controls.Add(this.mskbxAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblAltura);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Calcular IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.MaskedTextBox mskbxAltura;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.MaskedTextBox mskbxPeso;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Atividade3/pIMC/pIMC/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mskbxAltura_Validated(object sender, EventArgs e)
+        {
+            double altura;
+            if (!double.TryParse(mskbxAltura.Text, out altura))
+                MessageBox.Show("número inválido");
+        }
+
+        private void mskbxPeso_Validated(object sender, EventArgs e)
+        {
+            double peso;
+            if (!double.TryParse(mskbxPeso.Text, out peso))
+                MessageBox.Show("número inválido");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Double altura;
+            Double peso;
+            if (!Double.TryParse(mskbxAltura.Text, out altura))
+            {
+                MessageBox.Show("número inválido");
+                mskbxAltura.Focus();
+            }
+            else
+               if (!Double.TryParse(mskbxPeso.Text, out peso))
+            {
+                MessageBox.Show("número inválido");
+                mskbxPeso.Focus();
+            }
+            else
+                if (altura <= 0)
+            {
+                MessageBox.Show("Altura não pode ser menor ou igual a Zero");
+                mskbxAltura.Focus();
+            }
+            else
+                if (peso <= 0)
+            {
+                MessageBox.Show("Peso não pode ser menor ou igual a Zero");
+                mskbxPeso.Focus();
+            }
+            else
+            {
+                // Calculo
+                Double dobroAltura = altura * altura;
+
+                Double IMC = peso / dobroAltura;
+
+                if (IMC < 18.5) //Magreza
+                {
+                    MessageBox.Show("Seu IMC é :" + IMC.ToString("N2") + " É Menor que 18,5 e é classificado como Magreza e com Obesidade Grau 0");
+                }
+                else 
+                    if (IMC >= 18.5 && IMC < 25)
+                {
+                    MessageBox.Show("Seu IMC é :" + IMC.ToString("N2") + " Está entre 18,5 e 24,9 e é classificado como Normal e com Obesidade Grau 0");
+                }
+                else
+                    if (IMC >= 25 && IMC < 30)
+                {
+                    MessageBox.Show("Seu IMC é :" + IMC.ToString("N2") + " Está entre 25,0 e 29,9 e é classificado como Sobrepeso e com Obesidade Grau I");
+                }
+                else
+                    if (IMC >= 30 && IMC < 40)
+                {
+                    MessageBox.Show("Seu IMC é :" + IMC.ToString("N2") + " Está entre 30,0 e 39,9 e é classificado como Obesidade e com Obesidade Grau II");
+                }
+                else
+                    if (IMC >= 40)
+                {
+                    MessageBox.Show("Seu IMC é :" + IMC.ToString("N2") + " É maior que 40 e é classificado como Obesidade  Grave e com Obesidade Grau III");
+                }
+            }
+        }
+    }
+}
